3fdcebdfd4e6b4526a2ec8c7a9632830d25adc6b
jbed <jason@jbedford.net>
2016-02-10 13:21:09 -0800

    simple OLS regression

---
+++ 1_simple_linear_regression/linear_regression.py
@@ -0,0 +1,45 @@
+import theano
+import theano.tensor as T
+import numpy as np
+import matplotlib.pyplot as plt
+plt.ion()
+
+# create artificial training data
+x_train = np.linspace(-1, 1, 101)
+t_train = 2 * x_train + np.random.randn(*x_train.shape) * 0.33
+
+# plot data
+plt.scatter(x_train, t_train)
+
+# define symbolic Theano variables
+x = T.scalar()
+t = T.scalar()
+
+# define model: linear regression
+# no bias term
+def model(x, w):
+    return x * w
+
+w = theano.shared(0.0)
+y = model(x, w)
+
+cost = T.mean((t - y) ** 2)
+g = T.grad(cost, w)
+updates = [(w, w - g * 0.01)]
+
+
+# compile theano function
+train = theano.function([x, t], cost, updates=updates)
+
+
+# train model
+for ii in range(20):
+    print "iteration {}".format(ii + 1)
+    for x, t in zip(x_train, t_train):
+        train(x, t)
+    print "w = {}".format(w.get_value()) + '\n'
+
+
+# plot fitted line
+plt.plot(x_train, w.get_value() * x_train)
+
